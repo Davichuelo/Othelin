2a585d7b9cd9bdece06249d51fedf6d42071bbd5
Davichuelo <113983949+Davichuelo@users.noreply.github.com>
2022-11-22 12:09:54 +0100

    Carpetinha dia 1
    v1

---
+++ Macaquismo/src/edu/upc/epsevg/prop/othello/players/Macaquismo/PlayerMiniMax.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.Macaquismo;
+
+import edu.upc.epsevg.prop.othello.GameStatus;
+import edu.upc.epsevg.prop.othello.IAuto;
+import edu.upc.epsevg.prop.othello.IPlayer;
+import edu.upc.epsevg.prop.othello.Move;
+
+/**
+ *
+ * @author David
+ */
+public class PlayerMiniMax implements IPlayer, IAuto{
+
+    @Override
+    public Move move(GameStatus gs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void timeout() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}

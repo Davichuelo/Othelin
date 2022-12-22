92a7951269a431e728e45a4e2bf31c6c5e2ddc42
Davichuelo <113983949+Davichuelo@users.noreply.github.com>
2022-12-22 15:55:15 +0100

    v6
    acabado 🥱🥱🤧🤧

--- Macaquismo/src/edu/upc/epsevg/prop/othello/players/Macaquismo/PlayerID.java
+++ Macaquismo/src/edu/upc/epsevg/prop/othello/players/Macaquismo/PlayerID.java
@@ -51,7 +51,7 @@ public class PlayerID implements IPlayer, IAuto {
     //Constructor copiado de minimax
     public PlayerID(String nombre) { //solo el nombre en Game
         this._nombre = nombre;
-        this._profundidad = 1;
+        this._profundidad = 0;
         guardarTablero();
         guardar = new HashMap<>();
     }
@@ -65,10 +65,9 @@ public class PlayerID implements IPlayer, IAuto {
         macaquismoPlayer = gs.getCurrentPlayer();
         otroPlayer = CellType.opposite(macaquismoPlayer);
         
+        _profundidad = 0;
         
         movimiento = miniMax(gs); //no se le mete la profundidad
-        
-       
         
         return movimiento;
     }
@@ -88,7 +87,7 @@ public class PlayerID implements IPlayer, IAuto {
         long valHash = 0;
         for (int fila : filas){
             for (int columna : columnas){
-                if (nuevoTablero.getPos(fila, columna) != CellType.EMPTY){
+                if (nuevoTablero.devuelveOcupacion(fila, columna)){
                     valHash = valHash ^ (tabla[nuevoTablero.devuelveColor(fila,columna)][fila][columna]);
                 } 
             }
@@ -113,7 +112,9 @@ public class PlayerID implements IPlayer, IAuto {
     private Move miniMax(GameStatus T){
         int prof = 0;
         Move resultado = null;
+        Move anterior = null;
         while (!tiempo){
+            _profundidad++;
             int valorActual = Integer.MIN_VALUE;
             Point aux = new Point(0, 0);
             int alpha = Integer.MIN_VALUE, beta = Integer.MAX_VALUE;
@@ -124,7 +125,8 @@ public class PlayerID implements IPlayer, IAuto {
                 Point pos = movPosibles.get(i);
                 GameStatus auxT = new GameStatus(T);
                 auxT.movePiece(pos); //se supone que siempre es player1 :)
-                int nouValor = Min(auxT, prof-1, alpha, beta);
+                Integer nouValor = Min(auxT, prof-1, alpha, beta);
+                if (nouValor == null) return anterior; //movimiento anterior
                 if (nouValor > valorActual){
                     valorActual = nouValor;
                     // si falla fuera del puntin Point mov = new Point(fila,columna);
@@ -132,24 +134,35 @@ public class PlayerID implements IPlayer, IAuto {
                     aux = new Point(pos);
                 }
             }
+            anterior = resultado;
             resultado = new Move (aux, nodosExplorados, _profundidad, SearchType.MINIMAX_IDS);
-            //si se queda en medio sin tiempo que devuelva un null
+            
         }
         
         return resultado;
     }
     
-    private int Max(GameStatus T, int _profundidad, int alpha, int beta){
+    private Integer Max(GameStatus T, int _profundidad, int alpha, int beta){
         
-        int valorActual = Integer.MIN_VALUE;
+        Integer valorActual = Integer.MIN_VALUE;
         ArrayList<Point> movPosibles = T.getMoves();
         
         //mirar con el getMoves si es empty 
         
-        if (tiempo) return 5000000;
+        if (tiempo) return null;
         
-        else if (_profundidad == 0 || T.getEmptyCellsCount() == 0) //arreglar que no se pueda mover a ningun lafdo
-            valorActual = Heuristica(T);
+        else if (_profundidad == 0 || T.getEmptyCellsCount() == 0){ //arreglar que no se pueda mover a ningun lafdo
+            long valorHash = valorHash(T);
+            if (guardar.containsKey(valorHash)){
+                valorActual = guardar.get(valorHash);
+                //if (valorActual != Heuristica(T)) contadorErrores++;
+                heurAhorradas++;
+            }    
+            else {
+                valorActual = Heuristica(T);
+                guardar.put(valorHash, valorActual); //como llave el valor de Hash
+            } 
+        }
   
         else{
             for (int i = 0; i < movPosibles.size(); ++i){
@@ -157,8 +170,9 @@ public class PlayerID implements IPlayer, IAuto {
                 GameStatus auxT = new GameStatus(T); 
                 //_macaquismoColor = auxT.getCurrentPlayer();
                 auxT.movePiece(pos);
-                int fHMIN = Min(auxT, _profundidad-1, alpha, beta);
-                valorActual = Math.max(valorActual, fHMIN); 
+                Integer fHMIN = Min(auxT, _profundidad-1, alpha, beta);
+                if (fHMIN == null) return null;
+                valorActual = Math.max(valorActual, fHMIN);
                 alpha = Math.max(valorActual, alpha);
                 if(alpha >= beta) break;
 
@@ -168,23 +182,34 @@ public class PlayerID implements IPlayer, IAuto {
         return valorActual;
     }
     
-    private int Min(GameStatus T, int _profundidad, int alpha, int beta){
+    private Integer Min(GameStatus T, int _profundidad, int alpha, int beta){
         
-        int valorActual = Integer.MAX_VALUE;
+        Integer valorActual = Integer.MAX_VALUE;
         ArrayList<Point> movPosibles = T.getMoves();
         
         //mirar con el getMoves si es empty 
-        if (tiempo) return 5000000;
+        if (tiempo) return null;
         
-        else if (_profundidad == 0 || T.getEmptyCellsCount() == 0) //arreglar que no se pueda mover a ningun lafdo
-            valorActual = Heuristica(T);
+        else if (_profundidad == 0 || T.getEmptyCellsCount() == 0){ //arreglar que no se pueda mover a ningun lafdo
+            long valorHash = valorHash(T);
+            if (guardar.containsKey(valorHash)){
+                valorActual = guardar.get(valorHash);
+                //if (valorActual != Heuristica(T)) contadorErrores++;
+                heurAhorradas++;
+            }    
+            else {
+                valorActual = Heuristica(T);
+                guardar.put(valorHash, valorActual); //como llave el valor de Hash
+            } 
+        }
         
         else{
             for (int i = 0; i < movPosibles.size(); ++i){
                 Point pos = movPosibles.get(i); //xd
                 GameStatus auxT = new GameStatus(T); 
                 auxT.movePiece(pos);
-                int fHMAX = Max(auxT, _profundidad-1, alpha, beta);
+                Integer fHMAX = Max(auxT, _profundidad-1, alpha, beta);
+                if (fHMAX == null) return null;
                 valorActual = Math.min(valorActual, fHMAX); 
                 beta = Math.min(valorActual, beta);
                 if(alpha >= beta) break;
